260939f9a5158c18b60d9eae662cdbf2a57c0ed1
github username <github email address>
2021-03-02 16:26:21 -0800

    generate resource flashcards

---
+++ rails-api-backend/db/migrate/20210303002239_create_flashcards.rb
@@ -0,0 +1,12 @@
+class CreateFlashcards < ActiveRecord::Migration[6.0]
+  def change
+    create_table :flashcards do |t|
+      t.string :category
+      t.string :question
+      t.string :correct_answer
+      t.string :incorrect_answers
+
+      t.timestamps
+    end
+  end
+end
